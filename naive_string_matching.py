20327c734e736ad9b7a092c5d8a18373d5614331
chandu95059 <chandu95059@gmail.com>
2023-12-03 18:21:22 +0530

    first commit

---
+++ naive_string_matching.py
@@ -0,0 +1,15 @@
+heystack = 'abcxyz'
+needle = 'xyz'
+
+for i in range((len(heystack)-len(needle))+1):
+    flag = 0
+    for j in range(len(needle)):
+        if heystack[i+j]==needle[j]:
+            flag = 1
+        else:
+            flag = 0
+            break
+    if flag == 1:
+        print("match found at index:",i)
+if flag == 0:
+    print("Pattern Not Found")
